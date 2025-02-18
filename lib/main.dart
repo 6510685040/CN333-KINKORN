7f5b8a05ef9f7f26f11137a1b0c79504d36c5621
6510685057 <110814202+6510685057@users.noreply.github.com>
2025-02-18 14:50:30 +0700

    add contact us restaurant

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:kinkorn/Screen/home.dart';
 import 'package:kinkorn/Screen/login.dart';
 import 'package:kinkorn/restaurant/edit_profile.dart';
-import 'package:kinkorn/restaurant/language_setting.dart';  // ✅ แก้ไขให้ถูกต้อง
+import 'package:kinkorn/restaurant/language_setting.dart';
+import 'package:kinkorn/restaurant/contactus_restaurant.dart';
 
 
 void main() {
   runApp(MaterialApp(
-    home: LoginScreen(),
+    //home: LoginScreen(),
+    home: ContactUsRestaurant(), //
   ));
 }
 
