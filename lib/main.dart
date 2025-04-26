4a98e8b8733bcf35ac59e5307a320f0babd83497
Ponthipa Teerapravet <mild.ponthipa@gmail.com>
2025-04-26 12:28:55 +0700

    make user stay signed in

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:provider/provider.dart'; 
-
+import 'package:google_fonts/google_fonts.dart';
 import 'package:kinkorn/Screen/home.dart';
 import 'package:kinkorn/provider/cartprovider.dart';
-import 'package:google_fonts/google_fonts.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
@@ -15,9 +15,7 @@ void main() async {
     appleProvider: AppleProvider.deviceCheck,
   );
 
-  runApp(
-    const MyApp(), 
-  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -34,8 +32,8 @@ class MyApp extends StatelessWidget {
         home: const HomeScreen(),
         theme: ThemeData(
           textTheme: GoogleFonts.kanitTextTheme(
-            Theme.of(context).textTheme
-          )
+            Theme.of(context).textTheme,
+          ),
         ),
       ),
     );
